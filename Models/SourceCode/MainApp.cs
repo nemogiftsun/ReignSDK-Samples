79f2373482a19c5ad5530593099bc0a246546319
AndrewLinux <andrewlinux@andrewlinux-System-Product-Name.(none)>
2012-09-25 14:54:31 -0400

    Updated Model for Linux proj.

--- Models/SourceCode/MainApp.cs
+++ Models/SourceCode/MainApp.cs
@@ -60,7 +60,7 @@ namespace Demo_Windows
 				#elif XNA
 				video = Video.Create(VideoTypes.XNA, out videoType, root, this);
 				#elif OSX || LINUX
-				video = Video.Create(VideoTypes.OpenGL, out videoType, root, this, false);
+				video = Video.Create(VideoTypes.OpenGL, out videoType, root, this, true);
 				#elif iOS || ANDROID
 				video = Video.Create(VideoTypes.OpenGL, out videoType, root, this);
 				#endif
